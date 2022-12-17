afa15fcbc94fe56378f2909039771e76f51d7d53
Alusp <alusinejalloh800@gmail.com>
2022-12-17 20:31:20 +0200

    Resolve Linters Errors

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
   subject do
-    @user = User.create!(name: 'Tom',  email: "email@hmail.com", password: "password")
-    Recipe.create!(name: 'recipe 1', preparation_time: '5 hours', cooking_time: '20 min', description: 'ddajdhasjdhdjdhsadhadhjkahdkahd', user_id: @user.id)
+    @user = User.create!(name: 'Tom', email: 'email@hmail.com', password: 'password')
+    Recipe.create!(name: 'recipe 1', preparation_time: '5 hours', cooking_time: '20 min',
+                   description: 'ddajdhasjdhdjdhsadhadhjkahdkahd', user_id: @user.id)
   end
 
   it 'should be invalid without name' do
